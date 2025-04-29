450de736248ac4a392e9bf71a2faf8e4c811b48c
NevDev48 <nevriskayosia@gmail.com>
2025-04-29 21:06:45 +0700

    Add Tailwind CSS configuration with custom font families

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+      extend: {
+        fontFamily: {
+          aquire: ['Aquire', 'sans-serif'],
+          outfit: ['Outfit', 'sans-serif'],
+        },
+      },
+    },
+    plugins: [],
+  }
+  

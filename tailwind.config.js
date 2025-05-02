a8045917da01294ee9622fa05c8aebeff8e240f6
NevDev48 <nevriskayosia@gmail.com>
2025-05-02 12:22:41 +0700

    Refactor code structure for improved readability and maintainability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html"],
-    theme: {
-      extend: {
-        fontFamily: {
-          aquire: ['Aquire', 'sans-serif'],
-          outfit: ['Outfit', 'sans-serif'],
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        aquire: ['Aquire', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif'],
+      },
+      animation: {
+        blob: "blob 7s infinite",
+        float: 'float 3s ease-in-out infinite',
+      },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-10px)' },
+        },
+        blob: {
+          "0%, 100%": { transform: "translate(0px, 0px) scale(1)" },
+          "33%": { transform: "translate(30px, -50px) scale(1.1)" },
+          "66%": { transform: "translate(-20px, 20px) scale(0.9)" },
         },
       },
     },
-    plugins: [],
-  }
-  
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.animation-delay-2000': {
+          'animation-delay': '2s',
+        },
+        '.animation-delay-4000': {
+          'animation-delay': '4s',
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
+}
